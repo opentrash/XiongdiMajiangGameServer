8dc92d2e91496799eedcf15ddfac06ece4060e67
liyechen <gliyechen@hotmail.com>
2019-10-10 15:25:57 +0800

    chore: delete comments

--- sources/Tile.cpp
+++ sources/Tile.cpp
@@ -5,19 +5,6 @@
 #include "Tile.h"
 
 std::ostream &operator<<(std::ostream &os, const Tile &tile) {
-//    std::unordered_map<int, std::string> TileTypeNames {
-//            { BING, "BING" },
-//            { TIAO, "TIAO" },
-//            { WAN, "WAN" },
-//            { DONG, "DONG" },
-//            { NAN, "NAN" },
-//            { XI, "XI" },
-//            { BEI, "BEI" },
-//            { HONGZHONG, "HONGZHONG" },
-//            { FACAI, "FACAI" },
-//            { BAIBAN, "BAIBAN" },
-//            { FLOWER, "FLOWER"}
-//    };
     std::cout <<  "[" << TileTypeNames[tile._type] << "," << static_cast<int>(tile._num) << "]";
     return os;
 }
